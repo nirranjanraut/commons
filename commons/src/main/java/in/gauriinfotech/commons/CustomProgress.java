8d3aa4d372b699dea8b7b1143b50b2e5fb106277
Niranjan Raut <nirranjan.raut@gmail.com>
2016-10-02 22:14:18 +0530

    Removed static instance of CustomProgress.
    Removed static instance of CustomProgress which was leading to runtime exception.

--- commons/src/main/java/in/gauriinfotech/commons/CustomProgress.java
+++ commons/src/main/java/in/gauriinfotech/commons/CustomProgress.java
@@ -13,51 +13,42 @@ import android.content.DialogInterface;
  */
 public class CustomProgress extends ProgressDialog {
 
-    private static CustomProgress instance;
-
     private CustomProgress(Context context) {
         super(context);
     }
 
     public static CustomProgress from(Context context) {
-        if(instance == null) {
-            instance = new CustomProgress(context);
-            instance.setCancelable(true);
-        }
+        CustomProgress instance = new CustomProgress(context);
+        instance = new CustomProgress(context);
+        instance.setCancelable(true);
         return instance;
     }
 
     public CustomProgress cancelable(boolean cancelable) {
-        instance.setCancelable(cancelable);
-        return instance;
+        this.setCancelable(cancelable);
+        return this;
     }
 
     public CustomProgress onCancel(DialogInterface.OnCancelListener listener) {
-        instance.setOnCancelListener(listener);
-        return instance;
+        this.setOnCancelListener(listener);
+        return this;
     }
 
     public CustomProgress title(String title) {
-        instance.setTitle(title);
-        return instance;
+        this.setTitle(title);
+        return this;
     }
 
     public CustomProgress message(String message) {
-        instance.setMessage(message);
-        return instance;
+        this.setMessage(message);
+        return this;
     }
 
     public void display() {
-        if(instance.isShowing()) {
-            instance.dismiss();
+        if(this.isShowing()) {
+            this.dismiss();
         }
-        instance.show();
-    }
-
-    public static void close() {
-        if(instance != null && instance.isShowing()) {
-            instance.dismiss();
-        }
+        this.show();
     }
 
 }
