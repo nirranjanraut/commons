90ce5a925f6df475c49e15c30ff553f847c28793
Niranjan Raut <nirranjan.raut@gmail.com>
2016-10-02 10:14:19 +0530

    CustomProgress implemented.
    Implementation and testing of CustomProgress is done.

---
+++ commons/src/main/java/in/gauriinfotech/commons/CustomProgress.java
@@ -0,0 +1,63 @@
+package in.gauriinfotech.commons;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by NiRRaNjAN on 02/10/16.
+ * Contact email    :   nirranjan.raut@gmail.com
+ * Facebook         :   https://www.facebook.com/NiRRaNjAN.RauT
+ * Stackoverflow    :   http://stackoverflow.com/users/1911941/elite
+ * Blogs            :   http://technoscripts.com
+ */
+public class CustomProgress extends ProgressDialog {
+
+    private static CustomProgress instance;
+
+    private CustomProgress(Context context) {
+        super(context);
+    }
+
+    public static CustomProgress from(Context context) {
+        if(instance == null) {
+            instance = new CustomProgress(context);
+            instance.setCancelable(true);
+        }
+        return instance;
+    }
+
+    public CustomProgress cancelable(boolean cancelable) {
+        instance.setCancelable(cancelable);
+        return instance;
+    }
+
+    public CustomProgress onCancel(DialogInterface.OnCancelListener listener) {
+        instance.setOnCancelListener(listener);
+        return instance;
+    }
+
+    public CustomProgress title(String title) {
+        instance.setTitle(title);
+        return instance;
+    }
+
+    public CustomProgress message(String message) {
+        instance.setMessage(message);
+        return instance;
+    }
+
+    public void display() {
+        if(instance.isShowing()) {
+            instance.dismiss();
+        }
+        instance.show();
+    }
+
+    public static void close() {
+        if(instance != null && instance.isShowing()) {
+            instance.display();
+        }
+    }
+
+}
