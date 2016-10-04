4821cd137f97bb1698209ca7fe295b8df14eacc0
Niranjan Raut <nirranjan.raut@gmail.com>
2016-10-04 20:23:59 +0530

    Changed CustomProgress to Progress
    Changed CustomProgress class to Progress class.

--- commons/src/main/java/in/gauriinfotech/commons/Progress.java
+++ commons/src/main/java/in/gauriinfotech/commons/Progress.java
@@ -11,35 +11,35 @@ import android.content.DialogInterface;
  * Stackoverflow    :   http://stackoverflow.com/users/1911941/elite
  * Blogs            :   http://technoscripts.com
  */
-public class CustomProgress extends ProgressDialog {
+public class Progress extends ProgressDialog {
 
-    private CustomProgress(Context context) {
+    private Progress(Context context) {
         super(context);
     }
 
-    public static CustomProgress from(Context context) {
-        CustomProgress instance = new CustomProgress(context);
-        instance = new CustomProgress(context);
+    public static Progress from(Context context) {
+        Progress instance = new Progress(context);
+        instance = new Progress(context);
         instance.setCancelable(true);
         return instance;
     }
 
-    public CustomProgress cancelable(boolean cancelable) {
+    public Progress cancelable(boolean cancelable) {
         this.setCancelable(cancelable);
         return this;
     }
 
-    public CustomProgress onCancel(DialogInterface.OnCancelListener listener) {
+    public Progress onCancel(DialogInterface.OnCancelListener listener) {
         this.setOnCancelListener(listener);
         return this;
     }
 
-    public CustomProgress title(String title) {
+    public Progress title(String title) {
         this.setTitle(title);
         return this;
     }
 
-    public CustomProgress message(String message) {
+    public Progress message(String message) {
         this.setMessage(message);
         return this;
     }
